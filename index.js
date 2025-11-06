4591938e1d7be8b9f9ae68125f185aa42a20fe06
Aman Singh <amankumarkvm4@gmail.com>
2025-11-06 17:03:43 +0530

    Add form functionality and styles for task management
    - Implemented form for adding new tasks in add.ejs
    - Implemented form for updating tasks in update.ejs
    - Updated routes to handle POST requests for adding and updating tasks
    - Added path and url dependencies in package.json and package-lock.json
    - Created styles.css for consistent styling across the application

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import path from "path"
+
+import { fileURLToPath } from 'url'
 
 const app = express()
 
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 app.set('view engine','ejs')
+app.set("views", path.join(__dirname, "views"))
+
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, resp) => {
     resp.render('list')
@@ -14,6 +24,14 @@ app.get("/add", (req, resp) => {
 
 app.get("/update", (req, resp) => {
     resp.render('update')
+})
+
+app.post("/update", (req, resp) => {
+    resp.redirect('/')
+})
+
+app.post("/add", (req, resp) => {
+    resp.redirect('/')
 })
 
 app.listen(3200)
